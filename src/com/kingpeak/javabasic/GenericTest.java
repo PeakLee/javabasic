52d8f54028c75829aa3f1d1373162f83ea611e46
lixiaofeng <lixiaofeng@apusapps.com>
2018-11-20 17:59:15 +0800

    javabasic add 泛型demo

---
+++ src/com/kingpeak/javabasic/GenericTest.java
@@ -0,0 +1,9 @@
+package com.kingpeak.javabasic;
+
+public class GenericTest {
+
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+        System.exit(0);
+    }
+}
